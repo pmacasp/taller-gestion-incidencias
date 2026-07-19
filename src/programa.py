5fb41b4b4f7eb0e078cfc239e8d2d5791c1c7a0e
Pablo Macas <pmacasp@unemi.edu.ec>
2026-07-19 09:31:26 -0500

    Configurar reinicio de app.log en cada ejecucion

--- src/programa.py
+++ src/programa.py
@@ -12,7 +12,9 @@ logging.basicConfig(
     level=logging.INFO,
     format="%(asctime)s - %(levelname)s - %(message)s",
     datefmt="%Y-%m-%d %H:%M:%S",
-    encoding="utf-8"
+    encoding="utf-8",
+    filemode="w"
+
 )
 
 
